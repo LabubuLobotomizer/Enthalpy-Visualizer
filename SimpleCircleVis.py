535a4e69bf7cd839caa13b98e3c08581d3af6592
HenryMcanulty <henrymcanul@gmail.com>
2026-06-29 16:36:28 -0400

    BarelyWorks

--- SimpleCircleVis.py
+++ SimpleCircleVis.py
@@ -4,9 +4,6 @@ import random as rand
 
 pg.init
 
-#Starting the view at (0,0)
-camera_X = 0
-camera_Y = 0
 #Zoom base is the regular amount of pixels to be shown in the X direction, zoom factor is how zoomed out you are
 zoomBase = 1280
 zoomFactor = 1.0
@@ -15,6 +12,12 @@ gridSize = 150
 #Setting the size of the screen
 screenWidth = 1280
 screenHeight = 720
+#Setting a border for the circles to bounce around in, centered on 0,0
+borderWidth = screenWidth
+borderHeight = screenHeight
+#Starting the view at (0,0)
+camera_X = -1*screenWidth/2
+camera_Y = -1*screenHeight/2
 
 circlesList = []
 cameraViewX = camera_X + (screenWidth / 2) / zoomFactor
@@ -24,6 +27,49 @@ cameraViewY = camera_Y + (screenHeight / 2) / zoomFactor
 screen = pg.display.set_mode((screenWidth, screenHeight))
 pg.display.set_caption("Simple Circle Visualizer")
 clock = pg.time.Clock()
+
+class Circle():
+    def __init__(self, X_Pos, Y_Pos, Circle_Color, Radius, X_Velo, Y_Velo):
+        self.xPosition = X_Pos
+        self.yPosition = Y_Pos
+        self.xVelocity = X_Velo
+        self.yVelocity = Y_Velo
+        self.radius = Radius
+        self.color = Circle_Color
+    def getX(self):
+        return self.xPosition
+    def getY(self):
+        return self.yPosition
+    def getXV(self):
+        return self.xVelocity
+    def getYV(self):
+        return self.yVelocity
+    def getRadius(self):
+        return self.radius
+    def getColor(self):
+        return self.color
+    #This is assuming that the newX and newY are none by default unless explicitly changed, that way it is easy to update one at a time
+    def updatePos(self, NewX=None, NewY=None):
+        if NewX is not None:
+            self.xPosition = NewX
+        if NewY is not None:
+            self.yPosition = NewY
+    def updateVelo(self, NewXV=None, NewYV=None):
+        if NewXV is not None:
+            self.xVelocity = NewXV
+        if NewYV is not None:
+            self.yVelocity = NewYV
+    
+def drawBorder(BWidth, BHeight):
+    global cameraViewX, cameraViewY, zoomFactor
+    #Drawing Bottom Line
+    pg.draw.line(screen, (255,255,255), getScreenCoordinates(BWidth/2, BHeight/2, cameraViewX, cameraViewY, zoomFactor), getScreenCoordinates(-1*BWidth/2, BHeight/2, cameraViewX, cameraViewY, zoomFactor))
+    #Drawing Top Line
+    pg.draw.line(screen, (255,255,255), getScreenCoordinates(BWidth/2, -1*BHeight/2, cameraViewX, cameraViewY, zoomFactor), getScreenCoordinates(-1*BWidth/2, -1*BHeight/2, cameraViewX, cameraViewY, zoomFactor))
+    #Drawing Left Line
+    pg.draw.line(screen, (255,255,255), getScreenCoordinates(-1*BWidth/2, BHeight/2, cameraViewX, cameraViewY, zoomFactor), getScreenCoordinates(-1*BWidth/2, -1*BHeight/2, cameraViewX, cameraViewY, zoomFactor))
+    #Drawing Right Line
+    pg.draw.line(screen, (255,255,255), getScreenCoordinates(BWidth/2, BHeight/2, cameraViewX, cameraViewY, zoomFactor), getScreenCoordinates(BWidth/2, -1*BHeight/2, cameraViewX, cameraViewY, zoomFactor))
 
 def getScreenCoordinates(worldX, worldY, cameraX, cameraY, zoomFactor):
     #Calculating the screen coordinates based on the world coordinates, camera position, and zoom factor
@@ -95,10 +141,75 @@ def draw_grid(grid_size, zoom_factor, camera_x, camera_y, color: tuple = (int, i
 
 def drawCircles(CameraX, CameraY, ZoomFactor):
     global circlesList, screenWidth, screenHeight
-    for Circle in circlesList:
-        print()
-        pg.draw.circle(screen, Circle[2], (getScreenCoordinates(Circle[0], Circle[1], CameraX, CameraY, ZoomFactor)), Circle[3]*zoomFactor)
+    for CurrentCircle in circlesList:
+        #print(getScreenCoordinates(Circle.getX(CurrentCircle), Circle.getY(CurrentCircle), CameraX, CameraY, ZoomFactor))
+        pg.draw.circle(screen, Circle.getColor(CurrentCircle), (getScreenCoordinates(Circle.getX(CurrentCircle), Circle.getY(CurrentCircle), CameraX, CameraY, ZoomFactor)), Circle.getRadius(CurrentCircle)*zoomFactor)
 
+def updateCircles():
+    #Defining things up here for optimization
+    XVectorFlat = np.array([1 , 0])
+    YVectorFlat = np.array([0 , 1])
+    global circlesList
+    #First we check collision, but the second circle must make sure that it is not doing circles that are before the main circle in the list as to not double update teh collision
+    for QCircle in circlesList:
+        #Preparing a bunch of variables because itll be faster to prepare them than to call the functions a bunch
+        MainCircleX = Circle.getX(QCircle)
+        MainCircleY = Circle.getY(QCircle)
+        MainCircleR = Circle.getRadius(QCircle)
+        MainCircleMatrix = np.array([MainCircleX,MainCircleY])
+        MainCircleVelocityMatrix = np.array([Circle.getXV(QCircle), Circle.getYV(QCircle)])
+        #BCircle is the circle we are testing the main circle against before updating the main circle
+        for BCircle in circlesList:
+            if circlesList.index(BCircle) > circlesList.index(QCircle):
+                SecondaryCircleX = Circle.getX(BCircle)
+                SecondaryCircleY = Circle.getY(BCircle)
+                SecondaryCircleR = Circle.getRadius(BCircle)
+                #Checking the distance between the two to see if collision occurs
+                distanceBetweenCircles = np.sqrt(((MainCircleX-SecondaryCircleX)**2) + ((MainCircleY-SecondaryCircleY)**2))
+                if distanceBetweenCircles < MainCircleR+SecondaryCircleR:
+                    #Now we have to do that complicated ish math, For now everything is perfectly elastic
+                    SecondaryCircleMatrix = np.array([SecondaryCircleX,SecondaryCircleY])
+                    SecondaryCircleVelocityMatrix = np.array([Circle.getXV(BCircle), Circle.getYV(BCircle)])
+                    normalVector = (MainCircleMatrix - SecondaryCircleMatrix) #Getting difference between them
+                    normalVector = normalVector / np.linalg.norm(normalVector) #Converting it to normal vector
+                    #Some like math thing about rotating the interaction to be 90* hit not the actual angle of contact
+                    projection = np.dot(MainCircleVelocityMatrix - SecondaryCircleVelocityMatrix, normalVector)
+                    NewMainCircleVelocityMatrix = MainCircleVelocityMatrix - (projection * normalVector)
+                    NewSecondaryCircleVelocityMatrix = SecondaryCircleVelocityMatrix + (projection * normalVector)
+
+                    Circle.updateVelo(QCircle, NewXV=NewMainCircleVelocityMatrix[0], NewYV=NewMainCircleVelocityMatrix[1])
+                    Circle.updateVelo(BCircle, NewXV=NewSecondaryCircleVelocityMatrix[0], NewYV=NewSecondaryCircleVelocityMatrix[1])
+                    
+                    #Here we also have to separate overlapped circles
+                    overlap = MainCircleR+SecondaryCircleR - np.sqrt(((MainCircleX-SecondaryCircleX)**2) + ((MainCircleY-SecondaryCircleY)**2))
+                    if MainCircleX > SecondaryCircleX:
+                        Circle.updatePos(QCircle, NewX = MainCircleX+(normalVector[0]*overlap))
+                    else:
+                        Circle.updatePos(QCircle, NewX = MainCircleX-(normalVector[0]*overlap))
+                    if MainCircleY > SecondaryCircleY:
+                        Circle.updatePos(QCircle, NewY = MainCircleY+(normalVector[1]*overlap))
+                    else:
+                        Circle.updatePos(QCircle, NewY = MainCircleY-(normalVector[1]*overlap))
+                    
+    #Now we check if circles are colliding with edges of our border
+    global borderWidth, borderHeight
+    for CCircle in circlesList:
+        if Circle.getX(CCircle) > borderWidth/2:
+            Circle.updateVelo(CCircle, NewXV=Circle.getXV(CCircle)*-1)
+        if Circle.getX(CCircle) < -1*borderWidth/2:
+            Circle.updateVelo(CCircle, NewXV=Circle.getXV(CCircle)*-1)
+        if Circle.getY(CCircle) < -1*borderHeight/2:
+            Circle.updateVelo(CCircle, NewYV=Circle.getYV(CCircle)*-1)
+        if Circle.getY(CCircle) > 1*borderHeight/2:
+            Circle.updateVelo(CCircle, NewYV=Circle.getYV(CCircle)*-1)
+    #Now we finally actually move the circles
+    for PCircle in circlesList:
+        Circle.updatePos(PCircle, NewX=Circle.getX(PCircle)+Circle.getXV(PCircle), NewY=Circle.getY(PCircle)+Circle.getYV(PCircle))
+
+
+
+
+                    
 
 
 #Running makes sure we didn't cancel
@@ -112,7 +223,10 @@ while running:
 
     screen.fill((40, 40, 45))  # Clear the screen with black
     draw_grid(gridSize, zoomFactor, cameraViewX, cameraViewY, (0, 0, 0)) #Call the function above
+    drawBorder(borderWidth, borderHeight)
+    updateCircles()
     drawCircles(cameraViewX, cameraViewY, zoomFactor) #Draws all the circles
+    pg.draw.circle(screen, (255,0,0), getScreenCoordinates(0, 0, cameraViewX, cameraViewY, zoomFactor), 2)
 
 
     pg.display.flip()  # Update the display
@@ -144,5 +258,4 @@ while running:
         if event.type == pg.MOUSEBUTTONDOWN:
             if event.button == 1:
                 circleWorld = getWorldCoordinates(pg.mouse.get_pos()[0], pg.mouse.get_pos()[1], cameraViewX, cameraViewY, zoomFactor)
-                circlesList.append([circleWorld[0], circleWorld[1], (rand.randint(0,255), rand.randint(0,255), rand.randint(0,255)), rand.randint(10, 600)])
-                print(circleWorld)
+                circlesList.append(Circle(circleWorld[0], circleWorld[1], (rand.randint(0,255), rand.randint(0,255), rand.randint(0,255)), 15, rand.randint(-10, 10), rand.randint(-10, 10)))
